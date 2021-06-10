b2ff502e1999bb2f1c4e7301654d6a115a6ece56
dihan48 <dihan4894@mail.ru>
2021-06-10 11:14:26 +0300

    изменение адреса API на filltext.herokuapp.com

--- src/api/tableApi.js
+++ src/api/tableApi.js
@@ -1,6 +1,12 @@
+/*
+    SSL сертификат сайта filltext.com выдан для домена *.herokuapp.com 
+    DNC запись домена www.filltext.com: CNAME filltext.herokuapp.com
+    Для использования шифрования в запросах был изменеи исходный адрес API на filltext.herokuapp.com
+    Это ни как не влияет на работу приложения так как запросы идут на один и тот же сервер
+*/
 const urls = {
-    low: 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
-    high: 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+    low: 'https://filltext.herokuapp.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}',
+    high: 'https://filltext.herokuapp.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
 }
 const defaultUrl = urls.low;
 
